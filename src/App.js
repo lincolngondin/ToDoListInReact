9d16afbc5dde7a09b1cb91ec89f7a7f2a53d1304
Lincoln Monteiro <lincolngondim@gmail.com>
2021-11-19 21:10:51 -0300

    Criado o app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+
+const List = (props) => {
+	return (
+		props.list.map(
+			(v, index)=>{
+				return (
+					<div className="itemDiv">
+						<li className='item'>{v}</li>
+						<button className="itemDeleteButton" onClick={ (e)=>{props.remover(index)} }>x</button>
+					</div>
+					);
+			} 
+		)
+	);
+}
+
+const InputItem = (props) => {
+	return (
+		<form>
+			<div className="itemDiv">
+				<li className='item'>
+					<input onChange={(e)=>{props.handleInputChange(e.target.value)}} value={props.text} className="itemInput"></input>
+				</li>
+				<button className="itemAddButton" onClick={(e)=>{e.preventDefault();props.add(props.text);}}>+</button>
+			</div>
+		</form>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [text, setText] = useState('');
+	const [items, setItems] = useState(['kjk','kjl']);
+	
+	function handleInputChange(txt){
+		setText(txt);
+	}
+	
+	function addItem(item){
+		if(item !== ''){
+			setItems([...items, item]);
+			setText('');
+		}
+	}
+	
+	function removeItems(indice){
+		const temp = items.slice(0, indice).concat(items.slice(indice+1));
+		setItems(temp);
+	}
+	
+	return (
+		<div className="App">
+			<h1 className="title">To Do List </h1>
+			<ol>
+				<List list={items} remover={removeItems}/>
+				<InputItem handleInputChange={handleInputChange} add={addItem} text={text}/>
+			</ol>
+		</div>
+	);
 }
 
 export default App;
